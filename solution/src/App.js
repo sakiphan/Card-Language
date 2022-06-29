198ba8fd31d11c34acf1ea83de7231fbe9114188
Sakıp Han Dursun <100518315+sakiphan@users.noreply.github.com>
2022-06-29 22:38:21 +0300

    Add files via upload

---
+++ solution/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/header/Header';
+import './App.css';
+import Card from './components/card/Card';
+
+function App() {
+  return (
+    <div className="main-container">
+     <Header />
+     <Card />
+     
+
+    </div>
+  );
+}
+
+export default App;
